fd190dd251bd475600318638026cdadffa23e969
Jens Kolbenschlag <jkolbens@rhrk.uni-kl.de>
2021-05-22 01:26:01 +0200

    created binding

---
+++ bundles/org.openhab.binding.ws281x/src/main/java/org/openhab/binding/ws281x/internal/ws281xConfiguration.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ws281x.internal;
+
+/**
+ * The {@link ws281xConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Jens Kolb - Initial contribution
+ */
+public class ws281xConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname;
+    public String password;
+    public int refreshInterval;
+}
